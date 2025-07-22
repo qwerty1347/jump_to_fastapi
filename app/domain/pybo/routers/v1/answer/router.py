4a82451dd3c207744b05cb6fe76ea3e1004bd7ee
이건희 <qwertys1347@gmail.com>
2025-07-22 16:29:33 +0900

    2-07 답변 등록 (GET)
    answers, answer

--- app/domain/pybo/routers/v1/answer/router.py
+++ app/domain/pybo/routers/v1/answer/router.py
@@ -1,9 +1,45 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Path
+from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.domain.pybo.routers.v1.answer.services.service import AnswerService
+from databases.mysql.session import get_mysql_session
 
 
 router = APIRouter(prefix="/answer", tags=["Answer"])
+answer_service = AnswerService()
 
 
 @router.get('/')
-async def index():
-    return {"message": "Hello answer"}
+async def get_answers(
+    db: AsyncSession = Depends(get_mysql_session)
+) -> JSONResponse:
+    """
+    Answer 리스트를 가져오는 엔드포인트
+
+    Args:
+        db (AsyncSession): 비동기 데이터베이스 세션을 사용합니다.
+
+    Returns:
+        JSONResponse: Answer 리스트가 포함된 성공 응답을 반환합니다.
+    """
+    return await answer_service.get_answers(db)
+
+
+@router.get('/{answer_id}')
+async def get_answer(
+    answer_id: int = Path(...),
+    db: AsyncSession = Depends(get_mysql_session)
+) -> JSONResponse:
+    """
+    Answer 하나를 가져오는 엔드포인트
+
+    Args:
+        answer_id (int): Answer 하나의 고유 ID를 전달합니다.
+        db (AsyncSession): 비동기 데이터베이스 세션을 사용합니다.
+
+    Returns:
+        JSONResponse: Answer 하나가 포함된 성공 응답을 반환합니다.
+    """
+    return await answer_service.get_answer(db, answer_id)
--- app/domain/pybo/routers/v1/answer/services/service.py
+++ app/domain/pybo/routers/v1/answer/services/service.py
@@ -13,7 +13,50 @@ class AnswerService:
 
 
     async def get_answers_by_question_id(self, db: AsyncSession, question_id: int) -> JSONResponse:
+        """
+        특정 질문 ID에 해당하는 answer 목록을 가져오는 비동기 서비스
+
+        매개변수:
+        - db (AsyncSession): 비동기 데이터베이스 세션을 사용합니다.
+        - question_id (int): 특정 질문의 고유 ID를 전달합니다.
+
+        반환값:
+        - JSONResponse: 특정 질문에 해당하는 answer 목록이 포함된 성공 응답을 반환합니다.
+        """
         response = await self.answer_repository.get_answers_by_question_id(db, question_id)
         response_model = [AnswerItemResponse.model_validate(item) for item in response]
+
+        return success_response(jsonable_encoder(response_model))
+
+
+    async def get_answers(self, db: AsyncSession) -> JSONResponse:
+        """
+        answer 리스트를 가져오는 비동기 서비스
+
+        매개변수:
+        - db (AsyncSession): 비동기 데이터베이스 세션을 사용합니다.
+
+        반환값:
+        - JSONResponse: answer 리스트가 포함된 성공 응답을 반환합니다.
+        """
+        response = await self.answer_repository.get_answers(db)
+        response_model = [AnswerItemResponse.model_validate(item) for item in response]
+
+        return success_response(jsonable_encoder(response_model))
+
+
+    async def get_answer(self, db: AsyncSession, answer_id: int) -> JSONResponse:
+        """
+        answer 하나를 가져오는 비동기 서비스
+
+        매개변수:
+        - db (AsyncSession): 비동기 데이터베이스 세션을 사용합니다.
+        - answer_id (int): answer 하나의 고유 ID를 전달합니다.
+
+        반환값:
+        - JSONResponse: answer 하나가 포함된 성공 응답을 반환합니다.
+        """
+        response = await self.answer_repository.get_answer(db, answer_id)
+        response_model  = AnswerItemResponse.model_validate(response)
 
         return success_response(jsonable_encoder(response_model))
