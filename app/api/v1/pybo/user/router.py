91c9409f1cdf71c0779ba455bfdee66a3e6d0a14
이건희 <qwertys1347@gmail.com>
2025-07-28 17:36:37 +0900

    3-07 회원가입 (user 라우터)

--- app/api/v1/pybo/user/router.py
+++ app/api/v1/pybo/user/router.py
@@ -3,6 +3,12 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix="/user", tags=["User"])
 
+
 @router.get('/')
 async def index():
-    return {"message": "Hello user"}
+    return {"message": "Hello pybo-user"}
+
+
+@router.post('/form')
+async def create_user():
+    return {"message": "Hello create-user"}
