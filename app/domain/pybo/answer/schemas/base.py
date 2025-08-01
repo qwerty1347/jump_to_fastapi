f66f3a99c82282dff446b4274f0ba150796c45cb
이건희 <qwertys1347@gmail.com>
2025-08-01 17:42:41 +0900

    Pydantic v2 미사용 설정 제거

--- app/domain/pybo/answer/schemas/base.py
+++ app/domain/pybo/answer/schemas/base.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel, ConfigDict
 
 
 class AnswerBase(BaseModel):
-    model_config = ConfigDict(from_attributes=True, orm_mode=True)
+    model_config = ConfigDict(from_attributes=True)
--- app/domain/pybo/question/schemas/base.py
+++ app/domain/pybo/question/schemas/base.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel, ConfigDict
 
 
 class QuestionBase(BaseModel):
-    model_config = ConfigDict(from_attributes=True, orm_mode=True)
+    model_config = ConfigDict(from_attributes=True)
