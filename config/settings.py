253d0edc7a6c9bef8a29a7c830e69f36c901de63
이건희 <qwertys1347@gmail.com>
2025-08-06 17:44:13 +0900

    3-08 로그인과 로그아웃 (jwt 패키지 설치, env 설정)

--- config/settings.py
+++ config/settings.py
@@ -2,19 +2,29 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    APP_URL: str
+    
+    # MySQL 설정
     DB_HOST: str
     DB_PORT: int
     DB_DATABASE: str
     DB_USERNAME: str
     DB_PASSWORD: str
 
+    # MongoDB 설정
     MONGO_DB_PORT: int
 
     LOG_PATH: str
     UPLOAD_PATH: str
 
+    # OCR
     CLOVA_OCR_SECRET_KEY: str
     CLOVA_OCR_APIGW_INVOKE_URL: str
+
+    # JWT
+    PYBO_JWT_EXPIRE_MINUTES: int
+    PYBO_JWT_SECRET_KEY: str
+    PYBO_JWT_ALGORITHM: str
 
     class Config:
         env_file = ".env"
