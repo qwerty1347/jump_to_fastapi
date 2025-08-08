f52225c19b74ed9884e4db2f02314c2a6a7b6004
이건희 <qwertys1347@gmail.com>
2025-08-08 10:21:37 +0900

    refactor: try-except 변경

--- app/domain/ocr/services/service.py
+++ app/domain/ocr/services/service.py
@@ -1,36 +1,25 @@
 from fastapi import UploadFile
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
 
 from app.domain.ocr.schemas.response import OcrResponse
 from app.domain.ocr.modules.module import OcrModule
-from common.response import success_response, error_response
 
 
 class OcrService():
     def __init__(self):
         pass
 
-    async def handle_ocr(self, file: UploadFile, engine: str) -> JSONResponse:
+    async def handle_ocr(self, file: UploadFile, engine: str) -> OcrResponse:
         """
-        OCR 엔진을 동작하여 업로드된 이미지의 텍스트를 추출하는 함수
+        주어진 파일에 대해 지정된 OCR 엔진을 사용하여 텍스트를 추출하는 비동기 함수
 
         매개변수:
-        - file (UploadFile): 업로드된 이미지 파일
-        - engine (str): 사용할 OCR 엔진
+        - file (UploadFile): 텍스트를 추출할 업로드된 파일
+        - engine (str): 사용할 OCR 엔진의 이름
 
         반환값:
-        - JSONResponse: 추출된 텍스트 목록을 담은 JSON 응답
+        - OcrResponse: 추출된 텍스트를 포함하는 OCR 응답 객체
         """
-        try:
-            ocr_engine = OcrModule(engine)
-            ocr_result = await ocr_engine.recognize(file)
+        ocr_engine = OcrModule(engine)
+        ocr_result = await ocr_engine.recognize(file)
 
-            response = OcrResponse(
-                ocr_result = ocr_result
-            )
-
-            return success_response(jsonable_encoder(response))
-
-        except Exception as e:
-            return error_response(message=str(e))
+        return OcrResponse(ocr_result=ocr_result)
