7bfe9b2db57f469ce7fd2f279d93c141b73cb551
DESKTOP\LEE <test>
2025-08-09 17:21:08 +0900

    3-09 글쓴이 저장하기 (질문등록 user_id 추가)

--- app/domain/pybo/question/repositories/repository.py
+++ app/domain/pybo/question/repositories/repository.py
@@ -49,18 +49,18 @@ class QuestionRepository:
         return question
 
 
-    async def create_question(self, db: AsyncSession, item: dict) -> Question:
+    async def create_question(self, db: AsyncSession, data: dict) -> Question:
         """
-        Question을 생성하는 비동기 메서드
+        Question 하나를 생성하는 비동기 메서드
 
         매개변수:
         - db (AsyncSession): 비동기 데이터베이스 세션을 사용합니다.
-        - item (dict): Question 생성을 위한 데이터를 전달합니다.
+        - data (dict): Question 생성을 위한 데이터를 전달합니다.
 
         반환값:
         - Question: 생성된 Question 하나가 포함된 성공 응답을 반환합니다.
         """
-        question = Question(**item)
+        question = Question(**data)
         db.add(question)
         await db.flush()
         await db.refresh(question)
