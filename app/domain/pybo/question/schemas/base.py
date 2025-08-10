85d5ec9d822a448ae46aabb73484469800dbdb43
DESKTOP\LEE <test>
2025-08-10 14:33:00 +0900

    refactor question response 스키마 변경

--- app/domain/pybo/question/schemas/base.py
+++ app/domain/pybo/question/schemas/base.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel, ConfigDict
 
 class QuestionBase(BaseModel):
     model_config = ConfigDict(from_attributes=True)
+
+
+class QuestionResponseBase(BaseModel):
+    result: bool
+    code: int
